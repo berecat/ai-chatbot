fc30377dbbc044f74f7ed2ea3248635315c0efd9
dreemhai <dreemhai.wk@gmail.com>
2023-05-21 15:56:43 +0800

    added comments

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -9,6 +9,18 @@ __message_stream: Union[List[Dict[str, str]], List[Dict[str, str]]] = []
 
 
 def new_chat(mode, user_name, user_like, user_dislike, user_personality, user_gender, chatbot_gender):
+    """
+    This function is used to create a new chat with the chatbot
+    :param mode: The mode of the chat, either dating or friend
+    :param user_name: The name of the user
+    :param user_like: The like of the user
+    :param user_dislike: The dislike of the user
+    :param user_personality: The personality of the user
+    :param user_gender: The gender of the user
+    :param chatbot_gender: The gender of the chatbot
+    :return: None
+    """
+
     global __initial_prompt
     global __initial_role
     __message_stream.clear()
@@ -34,9 +46,16 @@ def new_chat(mode, user_name, user_like, user_dislike, user_personality, user_ge
         __initial_role = "friend"
 
     __message_stream.append({"role": __initial_role, "content": __initial_prompt})
+    pass
 
 
 def send_message(message):
+    """
+    This function is used to send a message to the chatbot
+    :param message: The message to be sent to the chatbot
+    :return: The response from the chatbot
+    """
+
     __message_stream.append({"role": "user", "content": message})
 
     response = openai.ChatCompletion.create(
@@ -51,4 +70,10 @@ def send_message(message):
 
 
 def get_message_stream(message_index):
+    """
+    This function is used to get the message stream
+    :param message_index:  The index of the message
+    :return: The content of the message at the index
+    """
+
     return __message_stream[message_index]["content"]
